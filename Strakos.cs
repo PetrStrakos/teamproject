1ae21d6464836e1de183acbda12a43ed94828991
Strakoš <strakos@netoffice.cz>
2022-01-31 10:47:29 +0100

    třída Strakos

---
+++ Strakos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Týmový_projekt
+{
+    class Strakos
+    {
+        public int Plat { get; set; }
+        public string Pozice { get; set; }
+    }
+}
