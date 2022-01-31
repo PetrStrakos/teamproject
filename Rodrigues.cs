9b8e6c19c6898ac0cec413967e4c80f9d3e4075e
Fabio Rodrigues <Rodrigues.skola@gmail.com>
2022-01-31 10:47:23 +0100

    Třída Rodrigues

---
+++ Rodrigues.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Týmový_projekt
+{
+    class Rodrigues
+    {
+        public int Cena { get; set; }
+        
+        public string Nazev { get; set; }
+    }
+}
